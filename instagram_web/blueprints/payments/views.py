033fd9be7ecde018e4bb6e0ebb53ee108b2c83a5
kevinchan950 <chanqx950120@hotmail.com>
2021-04-06 18:42:21 +0800

    added email auto reply

--- instagram_web/blueprints/payments/views.py
+++ instagram_web/blueprints/payments/views.py
@@ -2,9 +2,10 @@ from flask import request, render_template, Blueprint, url_for, flash, redirect
 from models.donation import Donation
 from models.image import Image
 from models.user import User
-from flask_login import current_user
+from flask_login import current_user, login_required
 import braintree
 import os
+import requests
 
 payments_blueprint = Blueprint('payments', __name__, template_folder="templates")
 
@@ -18,12 +19,14 @@ gateway = braintree.BraintreeGateway(
 )
 
 @payments_blueprint.route("/<username>/<imageid>")
+@login_required
 def payment_form(username,imageid):
     image = Image.get_by_id(imageid)
     client_token = gateway.client_token.generate()
     return render_template('payments/new.html', client_token=client_token, image=image)
 
 @payments_blueprint.route("/<username>/<imageid>/pay", methods=["POST"])
+@login_required
 def accept_payment(username, imageid):
     donation_amount = request.form.get("donation")
     nonce_from_client = request.form["nonce"]
@@ -38,6 +41,14 @@ def accept_payment(username, imageid):
     if result:
         donation = Donation(user=current_user.id , image = image, amount = donation_amount)
         donation.save()
+        
+        requests.post(
+        "https://api.mailgun.net/v3/sandbox64171c22c10a4c729763a53b5c9aa8c2.mailgun.org/messages",
+        auth=("api", os.getenv("MAILGUN_PRIVATE_KEY")),
+        data={"from": "Excited User <mailgun@sandbox64171c22c10a4c729763a53b5c9aa8c2.mailgun.org>",
+              "to": ["You", "chanqx950120@hotmail.com"],
+              "subject": f"Donation for {image.url}",
+              "text": f"An amount of {donation_amount} has been donated by {current_user.name}"})
         return redirect(url_for('users.show', username=image.user.name))
     else:
         flash("Something went wrong, please try again")   
--- models/donation.py
+++ models/donation.py
@@ -5,7 +5,7 @@ from models.image import Image
 import peewee as pw
 
 class Donation(BaseModel):
-    user = pw.ForeignKeyField(User, backref='donations')
-    image = pw.ForeignKeyField(Image, backref='donations')
+    user = pw.ForeignKeyField(User, backref='donations', on_delete="CASCADE")
+    image = pw.ForeignKeyField(Image, backref='donations', on_delete="CASCADE")
     amount = pw.DecimalField(decimal_places=2) 
     
